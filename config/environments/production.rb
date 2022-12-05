4e0a2c25169178fb112823d2ca6095866908a72a
darikmohammed <r.m.darik@gmail.com>
2022-12-05 16:00:16 +0300

    Update build

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Rails.application.config.hosts << "helpjuice-testproject-production-df19.up.railway.app"
 end
